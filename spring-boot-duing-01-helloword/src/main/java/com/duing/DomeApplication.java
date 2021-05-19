231191f33f23021c08350845cb3d354d3bb5972e
${xjq123456-ux} <${java_13585563479@163.com}>
2021-05-19 23:41:54 +0800

    第一次提交

---
+++ spring-boot-duing-01-helloword/src/main/java/com/duing/DomeApplication.java
@@ -0,0 +1,12 @@
+package com.duing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomeApplication.class,args);
+    }
+}
